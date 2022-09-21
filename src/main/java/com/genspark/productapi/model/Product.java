2e4542dc94d381c9937723682c369ccba8f7fd29
Ramaun <Ramaunkoomar@gmail.com>
2022-09-21 15:31:55 -0700

    Added product model.

---
+++ src/main/java/com/genspark/productapi/model/Product.java
@@ -0,0 +1,45 @@
+package com.genspark.productapi.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+public class Product {
+    @Id
+    private Long id;
+    private String name;
+    private double price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return id.equals(product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
