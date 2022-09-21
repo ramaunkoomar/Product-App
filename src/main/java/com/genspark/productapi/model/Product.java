14e41436cab144fd026f55e6b640bf33463a62eb
Ramaun <Ramaunkoomar@gmail.com>
2022-09-21 16:43:52 -0700

    Made product ID an auto-generated value.

--- src/main/java/com/genspark/productapi/model/Product.java
+++ src/main/java/com/genspark/productapi/model/Product.java
@@ -6,6 +6,7 @@ import java.util.Objects;
 @Entity
 public class Product {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private double price;
